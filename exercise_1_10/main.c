d055180abb91905b11653feb54e05cc952429105
Rihtarich <rihtarich.alexandr@gmail.com>
2019-07-28 18:43:21 +0300

    some tabs and backslashes

---
+++ exercise_1_10/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/* this makes tabs and backspaces visible */
+main()
+{
+	int c;
+	
+	while ((c = getchar()) != EOF) {
+		if (c == '\t') {
+			printf("\\t");
+		}
+		else if (c == '\b') {
+			printf("\\b");
+		}
+		else if (c == '\\') {
+			printf("\\\\");
+		}
+		else {
+			putchar(c);
+		}
+	}
+}
