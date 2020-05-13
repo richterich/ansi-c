8889d10218fc218f0c9bca740ba7ecfcd34c0c62
Rihtarich <rihtarich.ayu@gmail.com>
2020-05-13 23:49:34 +0300

    it is longer than 80 characters

---
+++ exercise_1_17/main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+#define MAXLINE 1000
+#define BOUNDARY 80
+
+int getLine(char[], int);
+void copy(char[], char[]);
+
+/* prints input lines that are
+ * longer than 80 characters */
+main()
+{
+	int len;			/* current line length */
+	char line[MAXLINE];	/* current input line */
+	while ((len=getLine(line,MAXLINE)) > 0) {
+		if (len >= BOUNDARY) {
+			printf("count: %d\nstring: %s", len, line);
+		}
+	}
+	return 0;
+}
+
+/* getLine: read a line into s, return length */
+int getLine(char s[], int lim)
+{
+	int c, i;
+
+	for (i = 0; i < lim-1 && (c=getchar())!=EOF && c != '\n';++i) {
+		s[i] = c;
+	}
+	if (c == '\n') {
+		s[i] = c;
+	}
+	s[i+1] = '\0';
+	return i;
+}
