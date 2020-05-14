11d597d2e54e220fefb388f5897e422f9f0553dd
Rihtarich <rihtarich.ayu@gmail.com>
2020-05-14 23:23:02 +0300

    trailing blanks and tabs

---
+++ exercise_1_18/main.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+#define MAXLINE 1000 /* maximum input line size */
+
+int getLine(char[], int);
+int trimEnd(char[], int);
+
+/* prints input lines that are
+ * longer than 80 characters */
+main()
+{
+    int len;            /* current line length */
+    char line[MAXLINE]; /* current input line */
+    while ((len=getLine(line, MAXLINE)) > 0) {
+        if ((len=trimEnd(line, len)) > 0) {
+            printf("chars: %d\ntrimed line: %s", len, line);
+        }
+    }
+    return 0;
+}
+
+/* getLine: read a line into s, return length */
+int getLine(char s[], int lim)
+{
+    int c, i;
+    for(i=0; i < lim-1 && (c=getchar())!=EOF && c!='\n'; ++i) {
+        s[i] = c;
+    }
+    if (c == '\n') {
+        s[i] = c;
+        ++i;
+    }
+    s[i] = '\0';
+    return i;
+}
+
+/* trimEnd: remove trailing blanks and tabs from s, return length */
+int trimEnd(char s[], int len)
+{
+    int c, i;
+    for (i=len-1; i >= 0 && ((c=s[i])=='\t' || c==' ' || c=='\n' || c=='\0'); --i) {
+        s[i] = '\0';
+    }
+    if (i >= 0 && i<len-1) {
+        ++i;
+        s[i] = '\n';
+    }
+    return i;
+}
